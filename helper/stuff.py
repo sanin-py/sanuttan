e802127b4b73cb8d1bbcbec72ff8181b5442d591
sanuttan <87420684+sanuttan@users.noreply.github.com>
2021-07-14 19:59:21 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -29,10 +29,11 @@ async def up(event):
 async def start(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos./nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties)./nYou can generate Samples and Screenshots of your video as well./nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'./n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
+
                Button.url("DEVELOPER", url="t.me/sanin_c"),
             ],
         ],
@@ -41,13 +42,13 @@ async def start(event):
 
 async def help(event):
     await event.reply(
-        "ini enthinado ninak Help? ithra paranjittum manasilaayilledey? Shavam!"
+        "ivde onnulla patiche 🙈🙉"
     )
 
 
 async def ihelp(event):
     await event.edit(
-        "ini enthinado ninak Help? ithra paranjittum manasilaayilledey? Shavam!",
+        "ivde onnulla patiche 🙈🙉",
         buttons=[Button.inline("BACK", data="beck")],
     )
 
@@ -55,11 +56,12 @@ async def ihelp(event):
 async def beck(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.edit(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos./nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties)./nYou can generate Samples and Screenshots of your video as well./nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'./n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
-            [               
-                Button.url("DEVELOPER", url="t.sanin_c"),
+            [
+                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
+                Button.url("DEVELOPER", url="t.me/danish_00"),
             ],
         ],
     )
