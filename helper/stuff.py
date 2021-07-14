b7e0b92fe395f180c244323f42c8c6a96bb1ca2e
sanuttan <87420684+sanuttan@users.noreply.github.com>
2021-07-14 18:52:09 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -29,7 +29,7 @@ async def up(event):
 async def start(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos./nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties)./nYou can generate Samples and Screenshots of your video as well./nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'./n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
@@ -55,12 +55,11 @@ async def ihelp(event):
 async def beck(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.edit(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos./nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties)./nYou can generate Samples and Screenshots of your video as well./nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'./n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
-            [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+            [               
+                Button.url("DEVELOPER", url="t.sanin_c"),
             ],
         ],
     )
