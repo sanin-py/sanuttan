7f38e941a5d9b1efcac919109a817b11d0032d82
sanuttan <87420684+sanuttan@users.noreply.github.com>
2021-07-14 20:18:35 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -29,11 +29,10 @@ async def up(event):
 async def start(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-
                Button.url("DEVELOPER", url="t.me/sanin_c"),
             ],
         ],
@@ -56,12 +55,11 @@ async def ihelp(event):
 async def beck(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.edit(
-        f"Hey, how are you chakkare? `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
+        f"Hey, `{ok.user.first_name}`\nThis is a CompressorBot which can encode videos.\nIt can compress your videos at a high percentage (normally 70%-80%, but it solely depends upon your video's size and other properties).\nYou can generate Samples and Screenshots of your video as well.\nLast but not the least, you can get all the basic details regarding your video under the tab 'MEDIA INFO'.\n✦ Enn ningalude swontham @sanin_c",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+               Button.url("DEVELOPER", url="t.me/sanin_c"),
             ],
         ],
     )
@@ -96,7 +94,7 @@ async def back(e):
 
 
 async def ccom(e):
-    await e.edit("Send Ur Custom Name For That File")
+    await e.edit("Send your custom name for that file")
     wah = e.pattern_match.group(1).decode("UTF-8")
     wh = decode(wah)
     out, dl, thum, dtime = wh.split(";")
@@ -111,7 +109,7 @@ async def ccom(e):
             g = repl.text + ".mkv"
         outt = f"encode/{chat}/{g}"
         x = await repl.reply(
-            f"Custom File Name : {g}\n\nSend Thumbnail Picture For it."
+            f"Custom File Name : {g}\n\nSend Thumbnail Picture for it."
         )
         replyy = cv.wait_event(events.NewMessage(from_users=chat))
         rep = await replyy
